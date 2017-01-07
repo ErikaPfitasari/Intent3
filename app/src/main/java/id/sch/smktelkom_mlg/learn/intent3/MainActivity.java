42751e785fbfef6a5a8f3b4441e920aa3fcffa61
ErikaPrafitasari <erikaprafitasari1@gmail.com>
2017-01-07 21:05:46 +0700

    Intent Browser

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -27,18 +27,35 @@ public class MainActivity extends AppCompatActivity {
                 composeSmsMessage("Pesan dari SMK Telkom Malanng");
             }
         });
+
+        //BROWSER
+        findViewById(R.id.imageViewBrowser).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openWebPage("http://www.smktelkom-mlg.sch.id");
+            }
+        });
     }
 
+    //Fungsi TELEPON
     public void dialPhoneNumber(String phoneNumber) {
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:" + phoneNumber));
         if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
     }
 
+    //FUNGSI SMS
     public void composeSmsMessage(String message) {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
         intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
+    }
+
+    //FUNGSI BROWSER
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
     }
 }
